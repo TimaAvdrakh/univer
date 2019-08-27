fbd8a3f9af34da628eeb85d02d2de8b95070adb6
augan93 <auganenu@gmail.com>
2019-08-27 16:52:51 +0600

    password field

--- portal_users/forms.py
+++ portal_users/forms.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField()
+    password = forms.CharField(
+        widget=forms.PasswordInput(),
+    )
