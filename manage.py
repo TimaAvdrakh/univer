d09fa2b2d3555e4962b88d12983e828e693913b4
augan93 <auganenu@gmail.com>
2019-12-03 10:24:18 +0600

    appender

--- manage.py
+++ manage.py
@@ -14,6 +14,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    from portal.settings import appender  # смотри в local_settings.txt
+    appender(sys)
+
     execute_from_command_line(sys.argv)
 
 
