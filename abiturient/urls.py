8c9e2b460095d78cf811df8442a2734d693a011d
Приб <pribka@gmail.com>
2020-01-27 14:28:27 +0600

    Added urlpattern for abiturient

---
+++ abiturient/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'abiturient'
+
+
+urlpatterns = [
+    # path('hz/',
+    #      views.StudentPresenceView.as_view(),
+    #      name='present_student'),
+]
