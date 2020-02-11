b002c1aa270cc528db7cebe74802717b37027317
Auganbay Rymkhan <37842023+Augan93@users.noreply.github.com>
2020-02-11 10:36:36 +0600

    my performance change2

--- send_advisors.py
+++ send_advisors.py
@@ -327,11 +327,16 @@ def find_dups_disc_code():
     '''
 
     query = '''
-                SELECT sd.student_id, sd.study_plan_uid_1c, sd.acad_period_id, sd.discipline_id,
-                       sd.load_type_id, sd.hours, sd.study_year_id,
+                SELECT sd.student_id, 
+                       sd.study_plan_uid_1c, 
+                       sd.acad_period_id, 
+                       sd.discipline_id,
+                       sd.load_type_id, 
+                       sd.hours, 
+                       sd.study_year_id,
                        COUNT(*) AS dup_count
                 FROM organizations_studentdiscipline sd
-                WHERE sd.study_year_id = 'c4f1122b-31f5-11e9-aa40-0cc47a2bc1bf'
+                WHERE sd.study_year_id = 'c4f1122b-31f5-11e9-aa40-0cc47a2bc1bf' AND sd.student_id = 'd2c75bf5-3552-11e9-aa40-0cc47a2bc1bf'
                 GROUP BY sd.student_id, sd.study_plan_uid_1c, sd.acad_period_id, sd.discipline_id,
                          sd.load_type_id, sd.hours, sd.study_year_id
                 HAVING COUNT(*) > 1
