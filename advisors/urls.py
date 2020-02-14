01e6f423af4133b23b0e80032f9c1bdacec3f84a
Auganbay Rymkhan <37842023+Augan93@users.noreply.github.com>
2020-02-14 17:38:33 +0600

    21235a

--- advisors/urls.py
+++ advisors/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
          name='registration_bid_list_copy'),
 
     path('student/disciplines/', views.StudentDisciplineListView.as_view(), name='discipline_short_list'),
+    path('student/disciplines/grouped/', views.StudentDisciplineGroupListView(),
+         name='student_discipline_group_list'),
 
     path('acad_periods/', views.AcadPeriodListView.as_view(), name='acad_periods_for_filter'),
     path('faculties/', views.FacultyListView.as_view(), name='faculty_list'),
