dde4d8b3885fdf89a2cd560079baeb49d7b60529
root_0892 <1903760@mail.ru>
2020-02-18 15:57:48 +0600

    edit PasswordChangeView

--- portal_users/serializers.py
+++ portal_users/serializers.py
@@ -218,8 +218,8 @@ class ProfileFullSerializer(serializers.ModelSerializer):
             is_employee = True
             teacher = models.Teacher.objects.get(profile=instance)
             # data['employee'] = TeacherSerializer(teacher).data
-            data['employee'] = 'Проверка'
             data.update(TeacherSerializer(teacher).data)
+            data['employee'] = 'Проверка'
             teacher_positions = models.TeacherPosition.objects.filter(profile=instance,
                                                                       is_active=True)
             data['positions'] = TeacherPositionSerializer(teacher_positions,
