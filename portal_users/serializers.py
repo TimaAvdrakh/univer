b17055b93ca06f76e44b5f9a732fd449506b4054
root_0892 <1903760@mail.ru>
2020-02-21 17:07:43 +0600

    NotRegisteredStudentListView

--- portal_users/serializers.py
+++ portal_users/serializers.py
@@ -1211,16 +1211,12 @@ class ProfileInterestsEditSerializer(serializers.ModelSerializer):
         )
 
     def update(self, instance, validated_data):
-        validated_data2 = validated_data
-        if validated_data.get('content'):
-            interests = validated_data2.pop('content')
-        else:
-            interests = validated_data2.get('interests')
+        interests = validated_data.get('interests')
 
         for interest in interests:
             models.Interest.objects.get_or_create(
                 profile=instance,
-                name=interest['name'],
+                name=interest['content'],
                 is_active=True,
             )
 
