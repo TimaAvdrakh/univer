73af326252a0c8489795685f2ac81a6c21327747
root_0892 <1903760@mail.ru>
2020-03-06 11:17:29 +0600

    Merge branch 'eldar_back' of https://github.com/AntonMezhanskiy/univer-back into bernar
    # Conflicts:
    #	advisors/serializers.py
    #	advisors/urls.py
    #	advisors/views.py

--- portal_users/views.py
+++ portal_users/views.py
@@ -901,17 +901,18 @@ class ChooseFormControlView(generics.UpdateAPIView):
                 data['status'] = request.data.status
             elif request.user.profile.role.is_supervisor:
                 data['status'] = 5
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
+
+            return Response(serializer.data)
         except:
-            pass
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
