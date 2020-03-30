3fcd831cb97674b93571107527786875889710b1
root_0892 <1903760@mail.ru>
2020-03-30 17:36:12 +0600

    edit RegisterStatisticsView

--- advisors/views.py
+++ advisors/views.py
@@ -869,23 +869,23 @@ class RegisterStatisticsView(generics.ListAPIView):
         page = self.paginate_queryset(distincted_queryset)
         for student_discipline in page:
             group_student_count = org_models.StudyPlan.objects.filter(
-                group_id=student_discipline['study_plan__group_id'],
+                group_id=student_discipline.get('study_plan__group_id'),
                 is_active=True,
             ).distinct('student').count()
 
             not_chosen_student_count = queryset.filter(
-                study_plan__group_id=student_discipline['study_plan__group_id'],
-                discipline_id=student_discipline['discipline_id']
+                study_plan__group_id=student_discipline.get('study_plan__group_id'),
+                discipline_id=student_discipline.get('discipline_id')
             ).distinct('student').count()
 
             d = {
-                'uid': student_discipline['uid'],
-                'faculty': student_discipline['study_plan__faculty__name'],
-                'cathedra': student_discipline['study_plan__cathedra__name'],
-                'speciality': student_discipline['study_plan__speciality__name'],
-                'group': student_discipline['study_plan__group__name'],
+                'uid': student_discipline.get('uid'),
+                'faculty': student_discipline.get('study_plan__faculty__name'),
+                'cathedra': student_discipline.get('study_plan__cathedra__name'),
+                'speciality': student_discipline.get('study_plan__speciality__name'),
+                'group': student_discipline.get('study_plan__group__name'),
                 'student_count': group_student_count,
-                'discipline': student_discipline['discipline__name'],
+                'discipline': student_discipline.get('discipline__name'),
                 'not_chosen_student_count': not_chosen_student_count,
                 'percent_of_non_chosen_student': (not_chosen_student_count / group_student_count) * 100,
                 }
