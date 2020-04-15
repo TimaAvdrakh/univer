951bf81671c7956ef5264c61c594272cf957798d
Евгений Долгов <Beattle-b@yandex.ru>
2020-04-15 16:14:34 +0300

    services

---
+++ services/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+@admin.register(models.Type)
+class TypeAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+    )
+    list_display = [
+        'uid',
+        'name',
+    ]
+
+@admin.register(models.SubType)
+class TypeAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+    )
+    list_display = [
+        'uid',
+        'name',
+        'type',
+        'example',
+    ]
+
+admin.site.register(models.Example)
---
+++ services/translation.py
@@ -0,0 +1,12 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import *
+
+
+@register(Type)
+class TypeTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+
+@register(SubType)
+class SubTypeTranslationOptions(TranslationOptions):
+    fields = ('name',)
