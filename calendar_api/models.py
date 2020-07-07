510956bfb5ce7a6bdfa0fc0bf4bd6c16ea0a726c
eldary89 <yerzhanov89@gmail.com>
2020-07-07 10:20:12 +0600

    Created models and migrations

--- calendar_api/models.py
+++ calendar_api/models.py
@@ -20,7 +20,76 @@ from portal_users.models import (
     Gender,
     Role
 )
+from schedules.models import (
+    Room
+)
 
 
+class RepetitionTypes(BaseCatalog):
+    class Meta:
+        verbose_name = 'Тип повторения события',
 
-# Create your models here.
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Events(BaseCatalog):
+    event_start = models.DateTimeField(
+        default=False,
+        verbose_name='Дата и время начала события',
+    )
+    event_end = models.DateTimeField(
+        default=False,
+        verbose_name='Дата и время конец события',
+    )
+    event_place = models.TextField(
+        default=False,
+        blank=True,
+        null=True,
+        verbose_name='Место проведения'
+    )
+    creator = models.ForeignKey(
+        Profile,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        verbose_name='Автор события',
+        related_name='events_creator'
+    )
+    participants = models.ManyToManyField(
+        Profile,
+        verbose_name='Участники события',
+        related_name='events_participants',
+
+    )
+    notification = models.BooleanField(
+        default=False,
+        verbose_name='Уведомления участникам',
+    )
+    event_description = models.TextField(
+        default=False,
+        blank=True,
+        null=True,
+        verbose_name='Описание события',
+    )
+    reserve_auditory = models.ForeignKey(
+        Room,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        verbose_name='Забранированная аудитория'
+    )
+    repetition_type = models.ForeignKey(
+        RepetitionTypes,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        verbose_name='Тип повторения события',
+    )
+
+    class Meta:
+        verbose_name = 'Событие'
+        verbose_name_plural = 'События'
+
+    def __str__(self):
+        return f'{self.name} созданно {self.creator.name}'
