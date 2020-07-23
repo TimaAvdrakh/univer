3bfbbcbb40ab35f1f15f74251b91760a3ea6b7e3
Maksat-Dossaliyev <maks_26_97@mail.ru>
2020-07-23 13:52:56 +0600

    UMK 3 step

--- emc/urls.py
+++ emc/urls.py
@@ -8,10 +8,8 @@ router = DefaultRouter()
 router.register(r'emc', views.EMCModelViewSet, 'emc')
 urlpatterns = router.urls
 
-#  = [
-#     path('create', views.CreateTeacherEMC.as_view(), name='create_emc'),
-#     path('list_th_one', views.EMCListOneTeacher.as_view(), name='emc_list_one_teacher'),
-#     path('list_th_one/<str:discipline>/', views.EMCListTeacherByDiscipline.as_view(),
-#          name='emc_list_teacher_by_discipline'),
-#     path('list_st', views.ListStudentEMC.as_view(), name='list_student_emc'),
-# ]
+urlpatterns += [
+    path('create', views.CreateTeacherEMC.as_view(), name='create_emc'),
+    path('list_th_one/<str:discipline>/', views.EMCListTeacherByDiscipline.as_view(),
+         name='emc_list_teacher_by_discipline'),
+]
