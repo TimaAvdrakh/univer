2eba3c48debac40a721c9c608ae06fa396a6d906
Alibek Kaparov <alibekkaparov@gmail.com>
2020-08-04 20:48:04 +0600

    Отработать создание/редактироване/фильтры по умолчанию

--- news/serializers.py
+++ news/serializers.py
@@ -6,9 +6,3 @@ class NewsSerializer(ModelSerializer):
     class Meta:
         model = News
         fields = '__all__'
-
-    def create(self, validated_data):
-        instance = super().create(validated_data)
-        # По фильтру с фронта вытащить ключи и по значениям вывести критерии 
-        request = self.context['request'].data
-        return instance
